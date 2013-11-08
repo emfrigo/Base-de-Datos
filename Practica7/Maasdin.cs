70305940dce1093f250d3b25311d79381c341fdc
emfrigo <emfrigo@hotmail.com>
2013-11-07 20:19:53 -0600

    Agregado de Menu

---
+++ Practica7/Maasdin.cs
@@ -0,0 +1,10 @@
+#region
+			//Alumnos alumnos = new Alumnos();
+			//alumnos.insertarRegistroNuevo("123456","Maria G");
+			//alumnos.editarNombreRegistro("19", "Maria G Palos");
+			//alumnos.mostrarTodos();
+			//alumnos.editarCodigoRegistro("19", "56789");
+			//alumnos.mostrarTodos();
+			//alumnos.eliminarRegistroPorId("19");
+			//alumnos.mostrarTodos();
+#endregion
--- Practica7/Main.cs
+++ Practica7/Main.cs
@@ -5,8 +5,27 @@ namespace Practica7
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
-		}
+		{	int sel;
+
+			Console.WriteLine(
+				"*****MENU*****\n"
+				+ "+ 1) Mostrar Todos\n"
+				+ "+ 2) Agregar Nuevo Usuario\n"
+				+ "+ 3) Editar Registro\n"
+				+ "+ 4) Elimianar Registro\n"
+				+ "+ 5) Salir"
+				);
+
+			sel = Convert.ToInt32(Console.ReadLine());
+
+			switch(sel){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			
+			}		}
 	}
 }
